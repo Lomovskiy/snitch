3572da5f1a947e47e06fcd4c69d24112d23c52d3
Alexander Lomovskiy <a@lomovskiy.com>
2021-03-23 18:50:07 +0300

    Added ListItem

--- app/src/main/java/com/lomovskiy/snitch/presentation/screen_passwords.kt
+++ app/src/main/java/com/lomovskiy/snitch/presentation/screen_passwords.kt
@@ -1,7 +1,7 @@
 package com.lomovskiy.snitch.presentation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -10,6 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -53,27 +56,71 @@ fun ScreenPasswords(viewModel: ScreenPasswordsViewModel) {
     val s = viewModel.getPasswords().collectAsState(initial = emptyList())
 
     Scaffold(
-        topBar = { AppBar(title = "Passwords") }
-    ) {
-        Box(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            LazyColumn {
-                items(s.value.size, itemContent = { idx ->
-                    ListItem(s.value[idx])
-                })
+        topBar = { AppBar(title = "Passwords") },
+        content = {
+            Box {
+                LazyColumn(
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(s.value.size, itemContent = { idx ->
+                        ListItem(s.value[idx], {})
+                    })
+                }
             }
+        },
+        floatingActionButton = {
             FloatingActionButton(
                 onClick = viewModel::addNewPassword
             ) {
                 Icon(Icons.Default.Add, contentDescription = null)
             }
         }
-    }
+    )
 
 }
 
+@Preview()
 @Composable
-fun ListItem(passwordEntity: PasswordEntity) {
-    Text(text = passwordEntity.login)
+fun ListItemPreview(
+    passwordEntity: PasswordEntity = PasswordEntity(
+        "a@lomovskiy.com",
+        "qwerty"
+    ),
+    onClick: () -> Unit = {}
+) {
+    ListItem(passwordEntity, onClick)
+}
+
+@Composable
+fun ListItem(passwordEntity: PasswordEntity, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
+        elevation = 4.dp
+    ) {
+        Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    style = TextStyle(color = Color.Gray),
+                    text = "Login:"
+                )
+                Text(
+                    modifier = Modifier.padding(start = 8.dp),
+                    text = passwordEntity.login
+                )
+            }
+            Row(modifier = Modifier.fillMaxWidth().padding(top = 8.dp)) {
+                Text(
+                    style = TextStyle(color = Color.Gray),
+                    text = "Password:"
+                )
+                Text(
+                    modifier = Modifier.padding(PaddingValues(start = 8.dp)),
+                    text = passwordEntity.password
+                )
+            }
+        }
+    }
 }
