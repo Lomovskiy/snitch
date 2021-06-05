10ea33ff4c2457aaff4043c218a061123f376e1e
Alexander Lomovskiy <a@lomovskiy.com>
2021-06-05 10:28:06 +0300

    Upgraded AGP version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta02")
+        classpath("com.android.tools.build:gradle:7.0.0-beta03")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${com.lomovskiy.snitch.Config.Versions.hiltCompiler}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${com.lomovskiy.snitch.Config.Versions.kotlin}")
     }
